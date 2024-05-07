4730be6bcca9623fba9aea05bf3730c1fce21feb
Ratansd <srstudio480@gmail.cm>
2024-05-07 17:41:20 +0600

    first commit

---
+++ src/dist/js/script.js
@@ -0,0 +1,20 @@
+document.querySelectorAll('.sidebar-dropdown-toggle').forEach(function(item){
+    item.addEventListener('click',function(e){
+        e.preventDefault()
+        const parent = item.closest('.group')
+        if(parent.classList.contains('selected')){
+            parent.classList.remove('selected')
+        }else{
+            document.querySelectorAll('.sidebar-dropdown-toggle').forEach(function(i){
+                i.closest('.group').classList.remove('selected')
+            })
+            parent.classList.add('selected')
+        }
+        
+    })
+})
+
+function toggleDropdown(){
+    let dropdown = document.querySelector('#dropdownButton #dropdown');
+    dropdown.classList.toggle('hidden');
+}
